9d46add58dd4c6985144b6217e0741ebc0ea8274
locojer <95993032+locojer@users.noreply.github.com>
2022-11-24 11:05:16 +0800

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -7,14 +7,10 @@ import "./global.css";
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
+                        //ok//
 
 root.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
